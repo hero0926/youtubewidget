6d39d25f1996fc7748e806cd266909735e443572
hero0926 <esistkalt@naver.com>
2018-03-08 13:27:14 +0900

    get current tab url

---
+++ youtubeTitle.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+from pywinauto import Desktop
+
+
+
+def FormatUrl(url):
+    if url and not url.startswith("https://"): 
+        return "http://" + url
+    return url
+
+def GetURL() :    
+
+    chrome_window = Desktop(backend="uia").window(class_name_re='Chrome')
+    chrome = chrome_window['Google Chrome']
+
+    address_bar_wrapper = chrome_window['Google Chrome'].main.Edit.wrapper_object()
+
+    url = address_bar_wrapper.legacy_properties()
+
+    print(url['Value'])
+
+def GetTitle() :
+
+    pass
