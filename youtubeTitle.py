fbd765f322f9b3d0082d45d7eca82c384031afa3
Ten <esistkalt@naver.com>
2018-03-08 14:30:11 +0900

    make path for different os

--- youtubeTitle.py
+++ youtubeTitle.py
@@ -1,24 +1,33 @@
 from __future__ import print_function
 from pywinauto import Desktop
-
-
+import platform
+import appscript
 
 def FormatUrl(url):
     if url and not url.startswith("https://"): 
         return "http://" + url
     return url
 
-def GetURL() :    
+def GetURL() :  
+    
+    if platform.system() == "Darwin" :
+        urls = appscript.app('Google Chrome').windows.tabs.URL()
+        print(platform)
+        
+    else :
+        urls = appscript.app('Google Chrome').windows.tabs.URL()
 
-    chrome_window = Desktop(backend="uia").window(class_name_re='Chrome')
-    chrome = chrome_window['Google Chrome']
+        chrome_window = Desktop(backend="uia").window(class_name_re='Chrome')
+        chrome = chrome_window['Google Chrome']
 
-    address_bar_wrapper = chrome_window['Google Chrome'].main.Edit.wrapper_object()
+        address_bar_wrapper = chrome_window['Google Chrome'].main.Edit.wrapper_object()
 
-    url = address_bar_wrapper.legacy_properties()
+        url = address_bar_wrapper.legacy_properties()
 
-    print(url['Value'])
+        print(url['Value'])
 
-def GetTitle() :
 
-    pass
+    
+    
+    def GetTitle() 
+:    pass
